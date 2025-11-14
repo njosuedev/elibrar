9600c2e7779720ccdb06ef73855d2ae377275566
njosuedev <njosuedev@gmail.com>
2025-11-14 13:34:28 +0200

    nfc

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,16 +1,68 @@
 import React from 'react'
-import {Field, Formik} from 'formik'
+import { Field, Formik, Form } from 'formik'
 
 function Create() {
-  return (
-    <div>
-        <Formik>
-            <form>
-                <Field placeholder="Title"/>
-            </form>
-        </Formik>
-    </div>
-  )
+
+    const header = {
+        textAlign: "center",
+        color: "blue"
+    }
+
+    const createPost = {
+        padding: "10px",
+        background: "#fdfcfcff",
+        border: "0.5px solid #e4e3e3ff",
+        width: "30vw",
+        justifySelf: "center",
+        marginTop: "5px",
+        borderRadius: "8px",
+    }
+
+    const formStyles = {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column"
+    }
+    const inpuCreate = {
+        paddingTop: "5px",
+        paddingBottom: "5px",
+        paddingLeft: "10px",
+        fontSize: "18px",
+        marginTop: "5px",
+        border: "0.5px solid #e4e3e3ff",
+        outline: "none",
+        resize: "none"
+    }
+    const submitBtn = {
+        padding: "10px",
+        marginTop: "10px",
+        border: "none",
+        background: "blue",
+        color: "#eee",
+        borderRadius: "20px",
+        fontSize: "15px",
+        fontWeight: "bold"
+    }
+
+    return (
+        <div style={createPost}>
+            <Formik>
+                <Form style={formStyles}>
+                    <h1 style={header}>Create Post</h1>
+                    <label>Title: </label>
+                    <Field style={inpuCreate} id="inpuCreate" name="title" />
+
+                    <label>Post: </label>
+                    <Field style={inpuCreate} id="inputCreate" as="textarea" rows="5" name="postText" />
+
+                    <label>User name: </label>
+                    <Field style={inpuCreate} id="inputCreate" name="username" />
+
+                    <button style={submitBtn} type='submit'>Create post</button>
+                </Form>
+            </Formik>
+        </div>
+    )
 }
 
 export default Create
