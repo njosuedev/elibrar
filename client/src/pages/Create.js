d877d8bedf2c3d6529044a43aaf0e9bf302462a7
njosuedev <njosuedev@gmail.com>
2025-11-14 14:28:49 +0200

    metro

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Field, Formik, Form } from 'formik'
+import { Field, Formik, Form, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+import axios from "axios";
+
 
 function Create() {
-
     const header = {
         textAlign: "center",
-        color: "blue"
-    }
+        color: "blue",
+    };
 
     const createPost = {
         padding: "10px",
@@ -16,13 +17,13 @@ function Create() {
         justifySelf: "center",
         marginTop: "5px",
         borderRadius: "8px",
-    }
+    };
 
     const formStyles = {
         width: "100%",
         display: "flex",
-        flexDirection: "column"
-    }
+        flexDirection: "column",
+    };
     const inpuCreate = {
         paddingTop: "5px",
         paddingBottom: "5px",
@@ -31,8 +32,8 @@ function Create() {
         marginTop: "5px",
         border: "0.5px solid #e4e3e3ff",
         outline: "none",
-        resize: "none"
-    }
+        resize: "none",
+    };
     const submitBtn = {
         padding: "10px",
         marginTop: "10px",
@@ -41,28 +42,81 @@ function Create() {
         color: "#eee",
         borderRadius: "20px",
         fontSize: "15px",
-        fontWeight: "bold"
+        fontWeight: "bold",
+    };
+
+    const span = {
+        color: "red"
     }
+
+    const initialValues = {
+        title: "",
+        postText: "",
+        username: ""
+    }
+
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3001/posts", data).then((response) => {
+            console.log("IT WORKS");
+        });
+    }
+
+    const validationSchema = Yup.object().shape({
+        title: Yup.string()
+            .required("Title is required"),
+
+        postText: Yup.string()
+            .required("Post text is required"),
+
+        username: Yup.string()
+            .min(3, "Username must be at least 3 characters")
+            .max(15, "Username cannot exceed 15 characters")
+            .required("Username is required")
+    });
 
     return (
         <div style={createPost}>
-            <Formik>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
                 <Form style={formStyles}>
                     <h1 style={header}>Create Post</h1>
+
                     <label>Title: </label>
-                    <Field style={inpuCreate} id="inpuCreate" name="title" />
+                    <ErrorMessage style={span} name="title" component="span" />
+                    <Field
+                        style={inpuCreate}
+                        autoComplete="off"
+                        id="inpuCreate"
+                        name="title"
+                    />
+
 
                     <label>Post: </label>
-                    <Field style={inpuCreate} id="inputCreate" as="textarea" rows="5" name="postText" />
+                    <ErrorMessage style={span} name="postText" component="span" />
+                    <Field
+                        style={inpuCreate}
+                        autoComplete="off"
+                        id="inputCreate"
+                        as="textarea"
+                        rows="5"
+                        name="postText"
+                    />
 
                     <label>User name: </label>
-                    <Field style={inpuCreate} id="inputCreate" name="username" />
+                    <ErrorMessage style={span} name="username" component="span" />
+                    <Field
+                        style={inpuCreate}
+                        autoComplete="off"
+                        id="inputCreate"
+                        name="username"
+                    />
 
-                    <button style={submitBtn} type='submit'>Create post</button>
+                    <button style={submitBtn} type="submit">
+                        Create post
+                    </button>
                 </Form>
             </Formik>
         </div>
-    )
+    );
 }
 
-export default Create
+export default Create;
